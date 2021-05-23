7804cadc94b199d67f32b4c87e7f26892240e519
www26 <mister.jorek@mail.ru>
2021-05-23 18:37:07 +0300

    Добавлена форма для изменения пользователей

---
+++ wwwroot/js/update.js
@@ -0,0 +1,12 @@
+﻿$('.openmodal').click(function (e) {
+    e.preventDefault();
+    $('.modal').addClass('opened');
+    $('.UserID').val($(this).find('.UID').text());
+    $('.UserMail').val($(this).find('.UMail').text());
+    $('.UserRole').val($(this).find('.URole').text());
+    $('.UserNick').val($(this).find('.UNick').text());
+});
+$('.closemodal').click(function (e) {
+    e.preventDefault();
+    $('.modal').removeClass('opened');
+});
